164bce97445acfc0a2b18d70c8c4e467fbe8cf5f
Hassanat Oladini <hass09@hotmail.co.uk>
2020-12-03 00:50:59 +0000

    search input action complete

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const image_path = "https://image.tmdb.org/t/p/w1280";
 const search_url =
   'https://api.themoviedb.org/3/search/movie?api_key=9886d43edf7edbf644d5931f936a10f4&query"';
 
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
 //GET MOVIES FUNCTION
 
 getMovies(api_url);
@@ -14,3 +17,17 @@ async function getMovies(url) {
 
   console.log(data.results);
 }
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const searchTerm = search.value;
+
+  if (searchTerm && searchTerm !== "") {
+    getMovies(search_api + searchTerm);
+
+    search.value = "";
+  } else {
+    window.location.reload();
+  }
+});
