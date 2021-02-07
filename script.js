ec0d9ef34034c2e7a5d244d905e7f003e6df31fe
HassanatOladini <58959440+HassanatOladini@users.noreply.github.com>
2021-02-07 00:12:02 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const discover_url =
-  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=9886d43edf7edbf644d5931f936a10f4&page=1";
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key= *insert key here* ;
 const search_url =
-  "https://api.themoviedb.org/3/search/movie?sort_by=popularity.desc&api_key=9886d43edf7edbf644d5931f936a10f4&page=1";
+  "https://api.themoviedb.org/3/search/movie?sort_by=popularity.desc&api_key= *insert key here*;
 const image_path = "https://image.tmdb.org/t/p/w1280";
 
 const main = document.getElementById("main");
